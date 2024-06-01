ba2f2eaa9e44ae376ba8a54c7308f76d4c2edaca
DonBrowny <kishore13ask@gmail.com>
2024-06-01 22:09:44 +0530

    fix: swipe card position

--- src/components/sms-swipe/sms-swipe.styles.ts
+++ src/components/sms-swipe/sms-swipe.styles.ts
@@ -1,35 +1,33 @@
 import { StyleSheet } from 'react-native'
 import { light } from '../../theme/color'
 
+const CARD_HEIGHT = 500
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-    flexDirection: 'column',
+  },
+  subContainer: {
+    flex: 1,
+    width: '100%',
+    zIndex: 1000,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   cardStyle: {
-    width: '100%',
-    height: 250,
+    height: CARD_HEIGHT,
     borderRadius: 24,
     borderWidth: 1,
     borderBlockColor: light.colors.primaryColor,
     backgroundColor: '#FFF',
   },
-  subContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   overlayLabelContainer: {
     width: '100%',
-    height: 250,
+    height: CARD_HEIGHT,
     borderRadius: 16,
   },
   progressView: {
-    height: 8,
     marginTop: 22,
     marginBottom: 16,
-    borderRadius: 12,
   },
 })
