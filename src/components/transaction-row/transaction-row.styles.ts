9580e09a7ebc97da86d053056b42597a80eba9fd
DonBrowny <kishore13ask@gmail.com>
2024-07-31 08:49:23 +0530

    fix:broken style

--- src/components/transaction-row/transaction-row.styles.ts
+++ src/components/transaction-row/transaction-row.styles.ts
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: 'row',
     columnGap: 12,
     alignItems: 'center',
