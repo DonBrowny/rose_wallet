7b5ebab7b197a32a70e42f4eed7ea7a9831f4f8e
DonBrowny <kishore13ask@gmail.com>
2024-09-03 08:47:33 +0530

    fix: reduce format to save space

--- src/utils/formatter/format-display-date.ts
+++ src/utils/formatter/format-display-date.ts
@@ -1,5 +1,5 @@
 export function formatDisplayDate(date: Date) {
-  const day = date.toLocaleDateString('en-GB', { month: 'long', day: 'numeric' })
-  const time = date.toLocaleTimeString('en-GB')
+  const day = date.toLocaleDateString('en-GB', { month: 'short', day: 'numeric' })
+  const time = date.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' })
   return `${day}-${time}`
 }
